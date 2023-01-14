1bddbf8195b4b4ab9ba9b09562ef30ace00ae287
Quanxi Li <liquanxi20z@ict.ac.cn>
2023-01-14 12:55:10 +0800

    change configure

--- conf/cassandra-env.sh
+++ conf/cassandra-env.sh
@@ -243,7 +243,7 @@ if [ "$LOCAL_JMX" = "yes" ]; then
   #JVM_OPTS="$JVM_OPTS -Dcassandra.jmx.local.port=$JMX_PORT"
   JVM_OPTS="$JVM_OPTS -Dcassandra.jmx.remote.port=$JMX_PORT"
   JVM_OPTS="$JVM_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
-  JVM_OPTS="$JVM_OPTS -Djava.rmi.server.hostname=10.208.130.26"
+  JVM_OPTS="$JVM_OPTS -Djava.rmi.server.hostname=127.0.0.1"
 else
   JVM_OPTS="$JVM_OPTS -Dcassandra.jmx.remote.port=$JMX_PORT"
   # if ssl is enabled the same port cannot be used for both jmx and rmi so either
